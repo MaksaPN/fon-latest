c655b8d0149fa1446e6ef8f47195f713c1cd9026
Milos Maksimovic <maksapn@gmail.com>
2019-04-06 02:36:30 +0200

    Ensured production build passing.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  newsApiUrl: 'http://cors-anywhere.herokuapp.com/https://mysterious-savannah-50993.herokuapp.com'
+  newsApiUrl: 'https://cors-anywhere.herokuapp.com/https://mysterious-savannah-50993.herokuapp.com'
 };
